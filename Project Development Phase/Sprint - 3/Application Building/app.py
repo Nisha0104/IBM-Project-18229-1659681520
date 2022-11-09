cf0634d4285be438e3b006604760e81c52dbc110
YUWARAJ02 <113450150+YUWARAJ02@users.noreply.github.com>
2022-11-09 18:49:13 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint - 3/Application Building/app.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 21:38:30 2022
+
+@author: home
+"""
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle as p
+app=Flask(__name__)
+model=p.load(open('CKD.pk1','rb'))
+@app.route('/')
+def HOME():
+    return render_template('home.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def prediction():    
+    aaa=request.form.values()
+    #a1=pd['blood_Urea'].astype(float)
+    data=[]
+    for x in aaa:
+        data.append(pd.to_numeric(x).astype(float))
+    #data.append(request.form.get('RBC'))
+    # a=[36,121,0,0,1,5.2,1,0]
+    # b=[float(x) for x in a]
+    features_value=[np.array(data)]  
+    features_name=['age','blood_urea','blood glucose random','coronary_artery_disease',
+                   'anemia','pus_cell','red_blood_cell','diabetesmellitus','pedal_edema']
+    df=pd.DataFrame(features_value, columns=features_name)
+    
+    output=model.predict(df)
+    if(output==0):
+        return render_template('index.html' , pred='Oops!! You have Kidney Chronic Disease. So, please concern a Doctor')
+    else:
+        return render_template('index.html' , pred='your are not affected by Chronic kidney Disease')
+    #print(aaa)
+    
+    # #return f"chronic, { b, output}"
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
